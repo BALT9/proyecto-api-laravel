362e9c46d1fcb48ffaadc6de9d5b57fa14de0bd2
willy <willybalt.2017@gmail.com>
2025-04-29 23:14:14 -0400

    se agrego el controlador perfil y cerrar sesion

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,19 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function funIngresar(Request $request){
-        //procesar
+        // validar 
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+        //autenticar
+        if(!Auth::attempt($credenciales)){
+            return response()->json(["mensaje" => "credenciales incorrectas"], 401);
+        }
+        // generar token 
+        $token = $request->user()->createToken('Token Auth')->plainTextToken;
+
+        return response()->json([
+            "access_token" => $token,
+            "usuario" => $request->user()
+        ],201);
     }
+
+
     public function funRegistro(Request $request){
+        // validar 
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email",
+            "password" => "required|same:cpassword"
+        ]);
         // guardar
+        $usuario = new User();
+        $usuario->name = $request->name;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password); //encripta contraseña
+
+        $usuario->save();
+
+        return response()->json(["mensaje" => "usuario Registrado"], 201);
+
     }
     public function funPerfil(Request $request){
         // procesar
+        return response()->json($request->user(), 201);
     }
     public function funSalir(Request $request){
         // procesar
+        $request->user()->tokens()->delete();
+        return response()->json(["mensaje"=>"Salio"], 201);
     }
 }
